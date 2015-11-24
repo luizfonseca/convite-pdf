cef0c8832ddce91f55d4983da28908ac9920a1d9
Luiz Fonseca <eu@luiz.cc>
2015-11-24 20:47:51 -0200

    Add first list of guests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+
+
+
+# Lista do dia 24/11
+# TODO: comentar o codigo até a proxima lista!
+lista1 = CSV.read(Rails.root.join('db/guests/lista1_1811.csv'), encoding: 'ISO8859-1')
+
+
+lista1.each do |guest|
+
+  if guest[0].blank?
+    next
+  end
+
+  Guest.where(name: guest[0].strip, email: guest[1].strip, scheduled_at: DateTime.strptime(guest[2], '%d/%m/%y')).first_or_create
+end
+
+
+
+# Lista do dia ____
+# TODO: comentar o codigo até a proxima lista!
+#lista2 = CSV.read(Rails.root.join('db/guests/lista2_2811.csv'), encoding: 'ISO8859-1')
+
+
+#lista2.each do |guest|
+
+  #if guest[0].blank?
+    #next
+  #end
+
+  #Guest.where(name: guest[0].strip, email: guest[1].strip, scheduled_at: DateTime.strptime(guest[2], '%d/%m/%y')).first_or_create
+  
+#end
+
+
+
